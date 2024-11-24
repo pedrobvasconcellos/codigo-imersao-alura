9d06b5d5a3db208b40d61078c982d9a9d8a90e5e
pedrobvasconcellos <vasconcellospedro213@gmail.com>
2024-11-23 22:37:10 -0300

    projeto insta-like-alura

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
+gcloud config set project codigo-imersao-alura
